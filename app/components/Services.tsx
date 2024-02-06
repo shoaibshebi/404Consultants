620d1836b260d1ad8b82e5d7a5f485a9ee6628ba
shoaibshebi <shoaib4030891@gmail.com>
2024-02-06 21:17:03 +0500

    updated services animations

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import { tilesData } from "../utils/data";
-import { slideIn, staggerContainer } from "../utils/motion";
+import { slideIn, staggerContainer, zoomIn } from "../utils/motion";
 
 export default function Services() {
   const numberOfStars = 10;
@@ -25,63 +25,51 @@ export default function Services() {
   const stars = Array.from({ length: numberOfStars }, (_, index) => (
     <img
       key={index}
-      src='/icons/star1.svg'
-      alt='Star'
-      className='tailwind-class'
+      src="/icons/star1.svg"
+      alt="Star"
+      className="tailwind-class"
       style={randomStyle()}
     />
   ));
 
   const PlanningTile = ({ img, title }: any) => (
-    <div className='rounded-xl w-full sm:w-fit p-4 [background:linear-gradient(93.65deg,_rgba(47,_128,_237,_0.1),_rgba(250,_13,_255,_0.1))] box-border h-[100px] flex flex-col items-start justify-center py-0 px-7 border-[1px] border-solid border-dodgerblue-200'>
-      <div className='flex flex-row items-center justify-start gap-[12px] rich-blacks-main relative z-20'>
-        <img className='relative w-14 h-14' alt='' src={img} />
-        <div className='relative leading-[32px] font-semibold'>{title}</div>
+    <div className="rounded-xl w-full sm:w-fit p-4  [background:linear-gradient(93.65deg,_rgba(47,_128,_237,_0.1),_rgba(250,_13,_255,_0.1))] box-border h-[100px] flex flex-col items-start justify-center py-0 px-7 border-[1px] border-solid border-dodgerblue-200">
+      <div className="flex flex-row items-center justify-start gap-[12px] rich-blacks-main relative z-20">
+        <img className="relative w-14 h-14" alt="" src={img} />
+        <div className="relative leading-[32px] font-semibold">{title}</div>
       </div>
     </div>
   );
 
   return (
     <motion.div
-      id='services'
+      id="services"
       variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
+      initial="hidden"
+      whileInView="show"
       viewport={{ once: true, amount: 0.25 }}
-      className='flex flex-col justify-center relative w-[80%] mx-auto z-10 mb-10 2xl:max-w-[1280px] overflow-scroll'
+      className="flex flex-col justify-center relative w-[80%] mx-auto z-10 mb-10 2xl:max-w-[1280px] overflow-scroll"
     >
       {stars}
-      <div className='relative text-5xl leading-[32px] font-semibold py-8'>
+      <div className="relative text-5xl leading-[32px] font-semibold py-8">
         Other Services
       </div>
 
-      <div className='flex flex-col gap-4'>
-        <motion.div
-          variants={slideIn("left", "tween", 0.2, 0.5)}
-          className='flex flex-col sm:flex-row gap-4 '
-        >
-          {tilesData.slice(0, 2).map((x, i) => (
-            <PlanningTile key={i} {...x} />
-          ))}
-        </motion.div>
-
-        <motion.div
-          variants={slideIn("right", "tween", 0.2, 0.5)}
-          className='flex flex-col sm:flex-row gap-4 items-center justify-center'
-        >
-          {tilesData.slice(2, 4).map((x, i) => (
-            <PlanningTile key={i} {...x} />
-          ))}
-        </motion.div>
-
-        <motion.div
-          variants={slideIn("left", "tween", 0.2, 0.5)}
-          className='flex flex-col sm:flex-row gap-4 items-center justify-end'
-        >
-          {tilesData.slice(4, 7).map((x, i) => (
-            <PlanningTile key={i} {...x} />
-          ))}
-        </motion.div>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col flex-wrap sm:flex-row gap-4 ">
+          {tilesData.map((x, i) => {
+            console.log("i", i);
+            return (
+              <motion.div
+                key={i}
+                className={`${i === 2 ? "ml-24" : ""} ${i === 4 ? "ml-4" : ""}`}
+                variants={zoomIn(0.4 * i, 1)}
+              >
+                <PlanningTile {...x} />
+              </motion.div>
+            );
+          })}
+        </div>
       </div>
     </motion.div>
   );
