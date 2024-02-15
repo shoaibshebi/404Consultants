a3e40ea8c572d1cd9d02aa31819533abd250def2
shoaibshebi <shoaib4030891@gmail.com>
2024-02-15 14:10:01 +0500

    fix secBtn issue

--- app/Reusable/SecBtn.tsx
+++ app/Reusable/SecBtn.tsx
@@ -4,7 +4,7 @@ import { TypingText } from "./customTexts";
 export default function SecBtn({ title }: any) {
   return (
     <div className="w-fit py-2 px-8 md:px-12  cursor-pointer flex justify-center items-center  bg-gradient-to-r from-[#3b82f61A] to-[#d946ef1A] rounded-[20px] border border-blue-500 border-opacity-10 shadow-blue-glow">
-      <TypingText title={title} textStyles="opacity-100" />
+      <TypingText isSpeedy={false} title={title} textStyles="opacity-100" />
     </div>
   );
 }
